d9fe289c28a2bb47fb8c47b09d1385072799fcfd
Richard Seward <raseward14@gmail.com>
2021-01-12 23:06:53 -0700

    inserted for loop in js

--- Develop/script.js
+++ Develop/script.js
@@ -9,7 +9,7 @@ const specialCharsAre = [' ', '!', '#', '$', '%', '&', '(', ')', '*', '+', ',', 
 function writePassword() {
   // Prompted to enter password length
   var passwordLength = window.prompt('How many characters do you want your password to be?');
-  
+  console.log(passwordLength);
   // Invalid length, end function
   if (passwordLength < 8 || length > 128) {
     alert('Valid passwords have greater than 8 and less than 128 characters.');
@@ -18,36 +18,57 @@ function writePassword() {
 
   // Valid length, prompt lowercase
   var lower = window.confirm('Include lowercase letters?');
-
   if (lower) {
-    parseInt.lowerCaseLettersAre;
+    var randomLower = lowerCaseLettersAre[Math.floor(Math.random()*lowerCaseLettersAre.length)];
+    console.log('include lowercase letters ' + randomLower);
+    // password.push(randomLower);
+  } else {
+    console.log('dont include lowercase letters.');
   }
 
   // prompt uppercase
   var upper = window.confirm('Include uppercase letters?');
   if (upper) {
-    .upperCaseLettersAre;
+    var randomUpper = upperCaseLettersAre[Math.floor(Math.random()*upperCaseLettersAre.length)];
+    console.log('include uppercase letters ' + randomUpper);
+    // password.push(randomUpper);
+  } else {
+    console.log('dont include uppercase letters.');
   }
 
   // prompt numbers
   var numbers = window.confirm('Include numbers?');
   if (numbers) {
-    .numericalCharsAre
+    var randomNumber = numericalCharsAre[Math.floor(Math.random()*numericalCharsAre.length)];
+    console.log('include numbers ' + randomNumber);
+    // password.push(randomNumber);
+  } else {
+    console.log('dont include numbers.');
   }
+
   // prompt special
   var special = window.confirm('Include special characters?');
   if (special) {
-    .specialCharsAre
+    var randomSpecial = specialCharsAre[Math.floor(Math.random()*specialCharsAre.length)];
+    console.log('include special characters ' + randomSpecial);
+    // password.push(randomSpecial);
+  } else {
+    console.log('dont include special characters.');
   }
-};
 
   // generate password
-  var password = generatePassword(Math.floor(Math.random() * passwordLength))
+  var password = generatePassword(); {
+    for (let index = 0; index < passwordLength; index++) {
+      if (password > passwordLength) break;
+      password.push(i);
+      const element = array[index];
+      
+    }
+  }
   var passwordText = document.querySelector("#password");
 
   window.alert('generate a password');
   passwordText.value = password;
-  return;
 
 }
 
